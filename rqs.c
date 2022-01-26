6887c6d46e87be95830dba1f29063b7627c1679d
shailender kumar 202120 <84677427+shailender2kumar@users.noreply.github.com>
2022-01-26 18:08:45 +0530

    Update rqs.c

--- rqs.c
+++ rqs.c
@@ -1,70 +1,49 @@
-#include <stdio.h>
-
-void quicksort (int [], int, int);
-
+#include<stdio.h>
+int partatin(int A[],int low,int high)
+{
+    int pivot=A[low];
+    int i= low+1;
+    int j= high;
+    int temp;
+    while(i<j){
+        while(A[i]<=pivot)
+        {
+            i++;
+        }
+        while(A[j]>pivot)
+        {
+            j--;
+        }
+        if(i<j){
+            temp=A[i];
+            A[i]=A[j];
+            A[j]=temp;
+        }
+    }
+    temp=A[low];
+    A[low]=A[j];
+    A[j]=temp;
+    return j;
+}
+void quicksort(int A[],int low ,int high)
+{
+int shortedindex ;
+if(low<high){
+    shortedindex= partatin(A,low,high);
+    quicksort(A,low,shortedindex -1);
+    quicksort(A,shortedindex+1,high);
+}
+}
+void printarray(int*A,int n){
+   for (int i=0;i<n;i++){
+    printf("%d",A[i]);
+}
+}
 int main()
 {
-    int list[7]={1,3,2,7,8,4,5};
-    int size =7;
-    int i;
-    int l;
-    for(l=0;l<i;l++){
-    printf("elements are %d\n",list[i]);
-    }
-   // printf("How many elements u want to Sort :: ");
-   // scanf("%d", &size);
-
-   // printf("\nEnter the elements below to be sorted :: \n");
-
-   // for (i = 0; i < size; i++)
-   // {
-       //  printf("\nEnter [ %d ] element :: ",i+1);
-        //scanf("%d", &list[i]);
-   // }
-
-    quicksort(list, 0, size - 1);
-
-    printf("\nAfter implementing Quick sort, Sorted List is :: \n\n");
-
-    for (i = 0; i < size; i++)
-    {
-        printf("%d  ", list[i]);
-    }
-
+    int A[]={1,4,7,2,5,8};
+    printarray(A,6);
     printf("\n");
-
-    return 0;
-}
-
-void quicksort(int list[], int low, int high)
-{
-    int pivot, i, j, temp;
-    if (low < high)
-    {
-        pivot = low;
-        i = low;
-        j = high;
-        while (i < j)
-        {
-            while (list[i] <= list[pivot] && i <= high)
-            {
-                i++;
-            }
-            while (list[j] > list[pivot] && j >= low)
-            {
-                j--;
-            }
-            if (i < j)
-            {
-                temp = list[i];
-                list[i] = list[j];
-                list[j] = temp;
-            }
-        }
-        temp = list[j];
-        list[j] = list[pivot];
-        list[pivot] = temp;
-        quicksort(list, low, j - 1);
-        quicksort(list, j + 1, high);
-    }
+      quicksort(A,0,5);
+     printarray(A,6);
 }
